58bb0929e31640ce671622ee5526449a8d247f3e
Parameswaran8 <paramsir8@gmail.com>
2025-08-28 00:42:14 +0530

    debug.keystore

--- App.tsx
+++ App.tsx
@@ -1,17 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from "@react-native/new-app-screen";
 import { StatusBar, StyleSheet, useColorScheme, View } from "react-native";
 import FeatureMain from "./src/FeatureMain/FeatureMain";
 import SplaceScreen from "./src/Onboard/Onboard";
 import LoginScreen from "./src/Authorization/Login";
 import WebViewContainer from "./src/WebViewContainer/WebViewContainer";
-// import Navigation from './src/Navigations/Navigation';
 
 function App() {
   const isDarkMode = useColorScheme() === "dark";
@@ -21,9 +12,9 @@ function App() {
       <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
       {/* <Navigation /> */}
       <SplaceScreen />
-      {/* <FeatureMain /> */}
-      {/* <LoginScreen /> */}
-      {/* <WebViewContainer /> */}
+      <FeatureMain />
+      <LoginScreen />
+      <WebViewContainer />
     </View>
   );
 }
--- src/Navigations/Navigation.tsx
+++ src/Navigations/Navigation.tsx
@@ -1,87 +1,14 @@
-// import React, { useEffect, useState } from 'react';
-// import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Navigation = () => {
+  return (
+    <View>
+      <Text>Navigation</Text>
+    </View>
+  );
+};
 
-// import SplaceScreen from '../Onboard/Onboard';
-// import FeatureMain from '../FeatureMain/FeatureMain';
-// import LoginScreen from '../Authorization/Login';
-// import WebViewContainer from '../WebViewContainer/WebViewContainer';
-// import { storage } from '../Context/storage';
+export default Navigation;
 
-// const Stack = createNativeStackNavigator();
-
-// const Navigation = () => {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   const [loading, setLoading] = useState(true); // for splash
-//   const [isFirstLaunch, setIsFirstLaunch] = useState<boolean | null>(null);
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-//   useEffect(() => {
-//     // simulate splash screen 3s
-//     setTimeout(async () => {
-//       try {
-//         const onboarded = await storage.getString('onboarded');
-//         setIsFirstLaunch(onboarded === null); // if null → first time
-//       } catch (e) {
-//         console.log(e);
-//         setIsFirstLaunch(true);
-//       }
-//       setLoading(false);
-//     }, 3000); // 3 seconds
-//   }, []);
-
-//   if (loading) {
-//     // show SplashScreen
-//     return (
-//       <View style={styles.splashContainer}>
-//         <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-//         <SplaceScreen />
-//       </View>
-//     );
-//   }
-
-//   return (
-//     <NavigationContainer>
-//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-//       <Stack.Navigator screenOptions={{ headerShown: false }}>
-//         {isFirstLaunch ? (
-//           // First launch → Onboarding
-//           <Stack.Screen name="Onboarding">
-//             {props => (
-//               <FeatureMain
-//                 {...props}
-//                 onFinish={async () => {
-//                   await storage.set('onboarded', 'true');
-//                   setIsFirstLaunch(false);
-//                 }}
-//               />
-//             )}
-//           </Stack.Screen>
-//         ) : !isLoggedIn ? (
-//           // If not logged in → Login
-//           <Stack.Screen name="Login">
-//             {props => (
-//               <LoginScreen
-//                 {...props}
-//                 onLoginSuccess={() => setIsLoggedIn(true)}
-//               />
-//             )}
-//           </Stack.Screen>
-//         ) : (
-//           // After login → Main App (WebView)
-//           <Stack.Screen name="WebView" component={WebViewContainer} />
-//         )}
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// };
-
-// export default Navigation;
-
-// const styles = StyleSheet.create({
-//   splashContainer: {
-//     flex: 1,
-//   },
-// });
+const styles = StyleSheet.create({});
